2764c63c0ae521e84138dd175edbe3c89c5ffebc
josedaniele <josedaniele493@gmail.com>
2024-04-23 20:12:02 -0300

    "tk-08: login html & css"

---
+++ sistema-gimnasio/src/Components/Layout/Layout.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './Header/Header'
+import NavBar from './NavBar/NavBar'
+import Login from './Login/Login'
+import Footer from './Footer/Footer'
+
+
+const Layout = () => {
+  return (
+    <>
+    <Header></Header>
+    <Login></Login>
+    <Footer></Footer>
+    </>
+  )
+}
+
+export default Layout
---
+++ sistema-gimnasio/src/Components/Login/Login.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from "react-bootstrap/Button";
+import "./Login.css";
+
+const Login = () => {
+  return (
+    <section className='login-section'>
+        <h2>BIENVENIDO/A</h2>
+        <Form  className="login-form-group">
+            <Form.Group className="mb-3" controlId="formGroupEmail">
+                <Form.Control className='input-login' type="email" placeholder="Email" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formGroupPassword">
+                <Form.Control className='input-login' type="password" placeholder="Contaseña"/>
+            </Form.Group>
+        </Form>
+        <Button variant="light" className='button-login'>Iniciar sesión</Button>
+        <div className='forget-password'>
+            No tenes cuenta?<a href="">Registrate</a> 
+        </div>
+
+    </section>
+  )
+}
+
+export default Login
