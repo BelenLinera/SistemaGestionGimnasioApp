837b8089f3b3ba4aea7cb628fffd06c4776115a4
josedaniele <josedaniele493@gmail.com>
2024-04-23 20:15:40 -0300

    tk-04: header

---
+++ sistema-gimnasio/src/Components/Layout/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Header.css";
+
+
+const Header = () => {
+  return (
+    <>
+    <header className="header">
+      <img src="/assets/logo-simple.png" alt="logo-gimnasio" className='logo' />
+      <h1>TRAINING CENTER</h1>
+    </header>
+    </>
+  )
+}
+
+export default Header
