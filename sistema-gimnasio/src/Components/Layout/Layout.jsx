6f445b68760ca07d854714b22a241ada6246d259
Belen Linera <belen.linera@gmail.com>
2024-05-16 16:22:02 -0300

    tk-03 abm de activity

--- sistema-gimnasio/src/Components/Layout/Layout.jsx
+++ sistema-gimnasio/src/Components/Layout/Layout.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import Admin from '../Admin/Admin'
 import {BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import FormUser from '../Forms/FormUser/FormUser';
+import FormActivity from '../Forms/FormActivity/FormActivity';
 import Client from '../Client/Client';
+import Activity from '../Activity/Activity';
 
 const Layout = () => {
   return (
@@ -23,6 +25,11 @@ const Layout = () => {
     <Route path="/client/create-client" element={<FormUser entity={"client"}/>} />
     <Route path= "/client" element={<Client/>}/>
     <Route path = "/client/edit-client/:userEmail" element={<FormUser entity={"client"} editForm={true}/>} />
+
+    <Route path="/activity" element={<Activity />}/>
+    <Route path="/activity/create-activity" element={<FormActivity entity={"activity"}/>} />
+    <Route path= "/activity" element={<Activity/>}/>
+    <Route path = "/activity/edit-activity/:activityName" element={<FormActivity entity={"activity"} editFormAct={true}/>} />
     </Routes>
     </Router>
     </>
