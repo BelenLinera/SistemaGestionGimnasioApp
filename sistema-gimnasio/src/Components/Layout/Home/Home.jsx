a144df08a26c80b5e6cfc73af8a6ca3931d9ae4b
Belen Linera <belen.linera@gmail.com>
2024-06-05 15:56:26 -0300

    home client a medias

--- sistema-gimnasio/src/Components/Layout/Home/Home.jsx
+++ sistema-gimnasio/src/Components/Layout/Home/Home.jsx
@@ -1,11 +1,52 @@
-import React, { useContext } from 'react'
+// components/Home.js
+import React, { useContext, useRef } from "react";
 import UserContext from '../../Context/UserContext';
+import { Link } from "react-router-dom";
+import { faUser, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./Home.css";
 
 const Home = () => {
-const { user } = useContext(UserContext);
-  return (
-    <div>{user?.email}</div>
-  )
-}
+  const homeRef = useRef();
+  const { user } = useContext(UserContext);
 
-export default Home
+  const showHomeNav = () => {
+    homeRef.current.classList.toggle("responsive_home");
+  };
+
+  return (
+      <body>
+        <div className="icon">
+          <FontAwesomeIcon icon={faUser} color="#000000" />
+          <h2>Hola {user?.userName}</h2>
+        </div>
+        
+        <nav ref={homeRef}>
+            <Link to="/editUser" className="link-home" onClick={showHomeNav}>
+              Mi cuenta
+            </Link>
+            <br/>
+            <Link to="/reservesUser" className="link-home" onClick={showHomeNav}>
+              Mis reservas
+            </Link>
+            <br/>
+            <Link to="/gymclass" className="link-home" onClick={showHomeNav}>
+              Clases
+            </Link>
+            <br/>
+            <Link to="/Activity" className="link-home" onClick={showHomeNav}>
+              Actividades
+            </Link>
+            <br/>
+            <Link to="/formMail" className="link-home" onClick={showHomeNav}>
+              Contáctanos
+            </Link>
+          
+          </nav>
+        <div>{user?.email}</div>
+      </body>
+    
+  );
+};
+
+export default Home;
