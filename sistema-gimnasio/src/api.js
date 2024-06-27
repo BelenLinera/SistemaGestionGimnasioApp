afc7fbdbc70a866ccab4bd6f948ae52007dc71f0
josedaniele <josedaniele493@gmail.com>
2024-06-27 19:03:24 -0300

    TK-07: styles in modal & api token by headers

--- sistema-gimnasio/src/api.js
+++ sistema-gimnasio/src/api.js
@@ -1,10 +1,23 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://localhost:7224',
-    headers: {
-        'Content-Type': 'application/json'
-    }
+  baseURL: 'https://localhost:7224',
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
+
+api.interceptors.request.use(
+  config => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (user && user.token) {
+      config.headers.Authorization = `Bearer ${user.token}`;
+    }
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
 
 export default api;
